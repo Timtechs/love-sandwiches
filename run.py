1bf247b2f2f3a2a35b00c277c51fa810e524d100
Timtechs <Timmy.s.deckert@gmail.com>
2022-09-21 14:17:32 +0000

    Refractor update worksheet functions into one

--- run.py
+++ run.py
@@ -56,26 +56,6 @@ def validate_data(values):
     return True
 
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
-def update_surplus_worksheet(data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-
 def update_worksheet(data, worksheet):
     """
     Recieves a list of integers to be inserted into a worksheet
